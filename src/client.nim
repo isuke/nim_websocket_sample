079eb1500b5a25b324f4c6b7f3b7f36d357209cb
isuke <isuke770@gmail.com>
2018-05-28 16:22:05 +0900

    WIP

--- src/client.nim
+++ src/client.nim
@@ -1,10 +1,19 @@
-import websocket, asyncnet, asyncdispatch
+import websocket, asyncnet, asyncdispatch, locks
 
 let ws = waitFor newAsyncWebsocketClient("localhost",
   Port 8080, "/?encoding=text", ssl = false, protocols = @["myfancyprotocol"])
 echo "connected!"
 
+var
+  writerTHread: Thread[void]
+  L: Lock
+
 proc reader() {.async.} =
+  while true:
+    let read = await ws.readData()
+    echo "read: ", read
+
+proc writer() {.async, thread.} =
   while true:
     let read = await ws.readData()
     echo "read: ", read
@@ -14,6 +23,10 @@ proc ping() {.async.} =
     await sleepAsync(6000)
     echo "ping"
     await ws.sendPing(masked = true)
+
+initLock(L)
+
+createThread(writerTHread, writer)
 
 asyncCheck reader()
 asyncCheck ping()
